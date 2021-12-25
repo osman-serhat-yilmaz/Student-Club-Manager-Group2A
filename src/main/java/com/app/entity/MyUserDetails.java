2fb6ffd1577547eb81ed38cdca68b79564dac66f
osman-serhat-yilmaz <serhat.yilmaz@ug.bilkent.edu.tr>
2021-12-25 18:32:41 +0300

    User entity updated, register, list clubs and show club data connections are done

--- src/main/java/com/app/entity/MyUserDetails.java
+++ src/main/java/com/app/entity/MyUserDetails.java
@@ -1,5 +1,6 @@
 package com.app.entity;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -11,11 +12,23 @@ public class MyUserDetails implements UserDetails {
     private String username;
     private String description;
 
+    private String department;
+    private int startOfStudies;
+    private Long dateOfBirth;
+    private String instagramUsername;
+    private String linkedinUsername;
+
     public MyUserDetails(User user) {
         this.email = user.getEmail();
         this.password = user.getPassword();
         this.username = user.getUsername();
         this.description = user.getDescription();
+
+        this.department = user.getDepartment();
+        this.dateOfBirth = user.getDateOfBirth();
+        this.startOfStudies = user.getStartOfStudies();
+        this.instagramUsername = user.getInstagramUsername();
+        this.linkedinUsername = user.getLinkedinUsername();
     }
 
     public MyUserDetails(){}
@@ -42,6 +55,26 @@ public class MyUserDetails implements UserDetails {
 
     public String getEmail() {
         return email;
+    }
+
+    public Long getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public int getStartOfStudies() {
+        return startOfStudies;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getInstagramUsername() {
+        return instagramUsername;
+    }
+
+    public String getLinkedinUsername() {
+        return linkedinUsername;
     }
 
     @Override
