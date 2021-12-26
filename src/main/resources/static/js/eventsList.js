300d663ab9b5274b7e2e7e581a51f97c7bc4ab9a
Serhat Gürgenyatağı <63667747+Se-Gu@users.noreply.github.com>
2021-12-26 05:12:12 +0300

    fixed the filter dropdown style in eventsList

--- src/main/resources/static/js/eventsList.js
+++ src/main/resources/static/js/eventsList.js
@@ -49,6 +49,13 @@ toggle between hiding and showing the dropdown content */
 function dropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
+function dropdown1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+}
+function dropdown2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
