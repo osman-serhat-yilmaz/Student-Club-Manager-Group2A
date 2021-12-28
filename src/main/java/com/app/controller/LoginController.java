1fdc6aa0af938e1437500d44024667b9c593ecc0
osman-serhat-yilmaz <serhat.yilmaz@ug.bilkent.edu.tr>
2021-12-28 23:10:52 +0300

    Registration bug fix and placeholder images added

--- src/main/java/com/app/controller/LoginController.java
+++ src/main/java/com/app/controller/LoginController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.security.sasl.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.sql.Date;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor(onConstructor = @__({@Autowired,@NonNull}))
@@ -25,6 +27,7 @@ public class LoginController {
 
     @RequestMapping("/login")
     public String index() {
+        System.out.println((new Date(System.currentTimeMillis()).toString()).replace("-", ""));
         return "/login";
     }
 
@@ -52,17 +55,46 @@ public class LoginController {
     @PostMapping("/register")
     public String registerUser(@RequestParam String username, @RequestParam String email, @RequestParam String password, @RequestParam String department,
                                @RequestParam String dateOfBirth, @RequestParam int startOfStudies, @RequestParam String instagramUsername,
-                               @RequestParam String linkedinUsername) throws Exception {
+                               @RequestParam String linkedinUsername, Model model) throws Exception {
+        List<User> users = userService.findAll();
+        for (User user: users)
+            if (email.equals(user.getEmail())) {
+                model.addAttribute("errorMessage", "User with this email already exists");
+                return "/register";
+            }
+
+        if (!email.contains("bilkent.edu.tr")) {
+            String errorMessage = "Email has to include 'bilkent.edu.tr'";
+            model.addAttribute("errorMessage", errorMessage);
+            return "/register";
+        }
+
         User user = new User();
         user.setEmail(email);
         user.setPassword(password);
         user.setDescription("");
         user.setUsername(username);
         user.setDepartment(department);
-        user.setDateOfBirth(Long.parseLong(dateOfBirth.replace("-", "")));
-        user.setStartOfStudies(startOfStudies);
-        user.setInstagramUsername(instagramUsername);
-        user.setLinkedinUsername(linkedinUsername);
+        if (Long.parseLong(dateOfBirth.replace("-", "")) < (Long.parseLong((new java.sql.Date(System.currentTimeMillis()).toString()).replace("-", "")) - 16)) {
+            user.setDateOfBirth(Long.parseLong(dateOfBirth.replace("-", "")));
+        }
+        else {
+            model.addAttribute("errorMessage", "Date of Birth should be at least 16 years less than today's date");
+            return "/register";
+        }
+
+        if (startOfStudies * 10000 <= Long.parseLong((new java.sql.Date(System.currentTimeMillis()).toString()).replace("-", "")) ) {
+            user.setStartOfStudies(startOfStudies);
+        }
+        else {
+            model.addAttribute("errorMessage", "Start of Studies should be less than today's date");
+            return "/register";
+        }
+
+        if (instagramUsername != "")
+            user.setInstagramUsername(instagramUsername);
+        if (linkedinUsername != "")
+            user.setLinkedinUsername(linkedinUsername);
         userService.save(user);
         return "login";
     }
